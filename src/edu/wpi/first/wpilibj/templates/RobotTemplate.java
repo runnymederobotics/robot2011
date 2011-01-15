7ea13d16e1ec0dd7669dacc873a926d447c6613d
unknown <daniel.pinero@.tdsb.on.ca>
2011-01-15 10:20:31 -0500

    Empty code for 2011

---
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -0,0 +1,42 @@
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.*;
+
+public class RobotTemplate extends IterativeRobot {
+
+    //Runs when the robot is turned
+    public void robotInit() {
+
+    }
+
+    //Runs at the beginning of autonomous period
+    public void autonomousInit() {
+
+    }
+
+    //Runs periodically during autonomous period
+    public void autonomousPeriodic() {
+
+    }
+
+    //Runs continuously during autonomous period
+    public void autonomousContinuous() {
+
+    }
+
+    //Runs at the beginning of teleoperated period
+    public void teleopInit() {
+
+    }
+
+    //Runs periodically during teleoperated period
+    public void teleopPeriodic() {
+
+    }
+
+    //Runs continuously during teleoperated period
+    public void teleopContinuous() {
+
+    }
+
+}
