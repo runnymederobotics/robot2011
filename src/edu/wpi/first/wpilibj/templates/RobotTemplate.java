c7e9b580680ba70fe37f90235bb3d35bdcc64143
unknown <Owner@.(none)>
2011-01-20 18:29:32 -0500

    started coding the elevator state machine

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -5,20 +5,48 @@ import edu.wpi.first.wpilibj.*;
 public class RobotTemplate extends IterativeRobot {
     
     static final double COUNTS_PER_METRE = 900.0;
+    static final double ELEVATOR_SPEED = 1.0;
+
+    static final int OPERATOR_GROUND_STATE = 1;
+    static final int OPERATOR_FEED_STATE = 2;
+    static final int OPERATOR_POSONE_STATE = 3;
+    static final int OPERATOR_POSTWO_STATE = 4;
+    static final int OPERATOR_POSTHREE_STATE = 5;
+    static final int OPERATOR_POSFOUR_STATE = 6;
+    static final int OPERATOR_POSFIVE_STATE = 7;
+    static final int OPERATOR_POSSIX_STATE = 8;
     
     boolean haveEncoders = true;
     
     Joystick stickRight = new Joystick(1);
     Joystick stickLeft = new Joystick(2);
 
+    Joystick stickOperator = new Joystick(3);
+
     Victor vicRight = new Victor(1);
     Victor vicLeft = new Victor(2);
+    Victor vicElevator = new Victor(3);
 
     PIDEncoder encRight = new PIDEncoder(1, 2);
     PIDEncoder encLeft = new PIDEncoder(3, 4);
 
+    Encoder encElevator = new Encoder(5, 6);
+
     PIDController pidRight = new PIDController(0.0, 0.00003, 0.0, encRight, vicRight, 0.005);
     PIDController pidLeft = new PIDController(0.0, -0.00003, 0.0, encLeft, vicLeft, 0.005);
+
+    class ElevatorState {
+        static final int ground = 0;
+        static final int feed = 1;
+        static final int posOne = 2;
+        static final int posTwo = 3;
+        static final int posThree = 4;
+        static final int posFour = 5;
+        static final int posFive = 6;
+        static final int posSix = 7;
+    }
+
+    int curElevatorState = ElevatorState.ground;
 
     //Runs when the robot is turned
     public void robotInit() {
@@ -51,13 +79,45 @@ public class RobotTemplate extends IterativeRobot {
 
     //Runs periodically during teleoperated period
     public void teleopPeriodic() {
-        System.out.println("rX: " + stickRight.getAxis(Joystick.AxisType.kX) + " | rY: " + stickRight.getAxis(Joystick.AxisType.kY));
-        System.out.println("lX: " + stickLeft.getAxis(Joystick.AxisType.kX) + " | lY: " + stickLeft.getAxis(Joystick.AxisType.kY));
+
+    }
+
+    //Eleveator state machine
+    public void elevatorStateMachine(double setPoint)
+    {
+        //Below setPoint, move up
+        if(encElevator.get() < setPoint)
+        {
+            vicElevator.set(ELEVATOR_SPEED);
+        }
+        //Above setPoint, move down
+        else if(encElevator.get() > setPoint)
+        {
+            vicElevator.set(-ELEVATOR_SPEED);
+        }
+        else
+        {
+            vicElevator.set(0.0);
+        }
     }
 
     //Runs continuously during teleoperated period
     public void teleopContinuous() {
-        //pidRight.setSetpoint(COUNTS_PER_METRE);
+        double setPoint = 0.0;
+
+        setPoint = stickOperator.getRawButton(OPERATOR_GROUND_STATE) ? ElevatorState.ground : setPoint;
+        setPoint = stickOperator.getRawButton(OPERATOR_FEED_STATE) ? ElevatorState.feed : setPoint;
+        setPoint = stickOperator.getRawButton(OPERATOR_POSONE_STATE) ? ElevatorState.posOne : setPoint;
+        setPoint = stickOperator.getRawButton(OPERATOR_POSTWO_STATE) ? ElevatorState.posTwo : setPoint;
+        setPoint = stickOperator.getRawButton(OPERATOR_POSTHREE_STATE) ? ElevatorState.posThree : setPoint;
+        setPoint = stickOperator.getRawButton(OPERATOR_POSFOUR_STATE) ? ElevatorState.posFour : setPoint;
+        setPoint = stickOperator.getRawButton(OPERATOR_POSFIVE_STATE) ? ElevatorState.posFive : setPoint;
+        setPoint = stickOperator.getRawButton(OPERATOR_POSSIX_STATE) ? ElevatorState.posSix : setPoint;
+
+        elevatorStateMachine(setPoint);
+
+        pidLeft.setSetpoint(stickLeft.getAxis(Joystick.AxisType.kY));
+        pidRight.setSetpoint(stickRight.getAxis(Joystick.AxisType.kY));
     }
 
 }
